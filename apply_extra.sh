29b81154821f75fa63898dacc1067b60541aeec1
Robin Lee <cheeselee@fedoraproject.org>
2019-12-30 00:09:07 +0800

    Update to 3.7.9
    - update runtime to 19.08
    - remove bundled lame

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-unzip xmind.zip fonts* plugins/org.xmind.cathy_3.7.8.201807240049.jar &>/dev/null
-unzip plugins/org.xmind.cathy_3.7.8.201807240049.jar -d cathy &>/dev/null
+unzip xmind.zip fonts* plugins/org.xmind.cathy_*.jar &>/dev/null
+unzip plugins/org.xmind.cathy_*.jar -d cathy &>/dev/null
 for size in 16 32 48 64 128 256 ; do
     mkdir -p export/share/icons/hicolor/${size}x${size}/apps ||:
     cp cathy/icons/xmind.${size}.png export/share/icons/hicolor/${size}x${size}/apps/net.xmind.XMind8.png
